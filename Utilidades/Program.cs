fb5a9b0b144b14777ef1346b5c350452170d075f
josuered4 <josuered4@gmail.com>
2023-01-23 23:00:04 -0600

    utilidades

---
+++ Utilidades/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Utilidades;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    static void realizarTarea()
+    {
+        // TODO: "to do" es un estandar para comentarios que los 
+        // lenguajes y los programadores utilizan para:
+            // + Donde continuar la codifcacion 
+            // + Tareas a realizar 
+            // + funciones incompletas 
+
+        //TO DO: en algunos editores de codigo o entornos de desarrllo
+        //nos mostraran todos los "to do" como si una lista de tareas 
+        // se tratase
+    }
+}
