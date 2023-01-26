0e70a8a620e92d58a183df1388c11ebbaa6fa789
josuered4 <josuered4@gmail.com>
2023-01-25 23:28:38 -0600

    Clases Anonimas

---
+++ ClasesAnonimas/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClaseAnonimas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Declaracion de clases anonimas 
+        var miClaseAnonima = new {Nombre = "Josue", Edad = 19 };
+        var miSegundaClaseAnonima = new {Nombre = "Isaac", Edad = 19 };
+        var miSegundaClaseAnonima = new {Nombre = "Isaac", Edad = "19", Peso = 2 };
+
+        //son como los objetos en js
+        //El compilador sabe que tipo de clase es 
+        //Si dos instancias tienen la mismo tipo, cantida y orden de campos, el compilador
+        //  los asignara del mismo tipo.
+        //Si son del mismo tipo puede reasignarse entre si.
+        //miClaseAnonima = miSegundaClaseAnonima
+
+        //Restrecciones.
+        //Solo puede tener campos publico 
+        //Todos los campos deben ser inicializados 
+        //Sin campos staticos
+        //Sin metodos 
+
+        Console.WriteLine("HOla");
+    }
+}
